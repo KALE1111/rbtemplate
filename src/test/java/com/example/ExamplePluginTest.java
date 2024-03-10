c52956d0bc883fdf2201123191cb4dcdae57c498
KALE1111 <135444858+KALE1111@users.noreply.github.com>
2024-03-10 14:04:41 -0700

    Initial commit

---
+++ src/test/java/com/example/ExamplePluginTest.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import com.example.EthanApiPlugin.EthanApiPlugin;
+import com.example.PacketUtils.PacketUtilsPlugin;
+import net.runelite.client.RuneLite;
+import net.runelite.client.externalplugins.ExternalPluginManager;
+
+public class ExamplePluginTest
+{
+	public static void main(String[] args) throws Exception
+	{
+		ExternalPluginManager.loadBuiltin(EthanApiPlugin.class, PacketUtilsPlugin.class);
+		RuneLite.main(args);
+	}
+}
